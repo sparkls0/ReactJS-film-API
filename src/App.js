77d3c121d1766d8b5cff169fda3bad44fb03202a
saltyfoxy <nerdgbx@gmail.com>
2020-07-18 17:48:05 +0200

    base API fonctionnelle exemple

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    post: {}
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        return response.json()
+      })
+      .then((result) => {
+        this.setState({post: result})
+        console.log(result)
+      })
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notre dernier article :</h1>
+      {this.state.post.title}
     </div>
   );
+};
 }
 
 export default App;
